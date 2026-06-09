b06cf73e193544e31817f1be7ef071ab84eaf01c
andrew <setyawanandrew74@gmail.com>
2026-06-09 16:23:17 +0700

    Refactor data preparation script: improve code formatting and readability by adding whitespace and aligning parameters

--- src/modeling/data preparation final.py
+++ src/modeling/data preparation final.py
@@ -35,9 +35,10 @@ train_set["income_cat"].value_counts() / len(train_set)
 for set_ in (train_set, test_set):
     set_.drop("income_cat", axis=1, inplace=True)
 
-housing=train_set.copy()
-housing_train= train_set.drop("median_house_value", axis=1)
+housing = train_set.copy()
+housing_train = train_set.drop("median_house_value", axis=1)
 housing_labels = train_set["median_house_value"].copy()
+
 
 class ClusterSimilarity(BaseEstimator, TransformerMixin):
     def __init__(self, n_clusters=10, gamma=1.0, random_state=None):
@@ -46,18 +47,27 @@ class ClusterSimilarity(BaseEstimator, TransformerMixin):
         self.random_state = random_state
 
     def fit(self, X, y=None, sample_weight=None):
-        self.kmeans_ = KMeans(n_clusters=self.n_clusters, random_state=self.random_state)
+        self.kmeans_ = KMeans(
+            n_clusters=self.n_clusters, random_state=self.random_state
+        )
         self.kmeans_.fit(X, sample_weight=sample_weight)
-        return self #always return self in fit method
+        return self  # always return self in fit method
+
     def transform(self, X):
         return rbf_kernel(X, self.kmeans_.cluster_centers_, gamma=self.gamma)
+
     def get_feature_names_out(self, names=None):
         return [f"cluster_similarity_{i}" for i in range(self.n_clusters)]
 
+
 def column_ratio(X):
     return X[:, [0]] / X[:, [1]]
+
+
 def ratio_names(function_transformer, feature_names_in):
     return ["ratio"]
+
+
 def ratio_pipeline():
     return make_pipeline(
         SimpleImputer(strategy="median"),
@@ -65,33 +75,45 @@ def ratio_pipeline():
         StandardScaler(),
     )
 
+
 log_pipeline = make_pipeline(
     SimpleImputer(strategy="median"),
     FunctionTransformer(np.log, feature_names_out="one-to-one"),
-    StandardScaler())
+    StandardScaler(),
+)
 
 cat_pipeline = make_pipeline(
-    SimpleImputer(strategy="most_frequent"),
-    OneHotEncoder(handle_unknown="ignore")
+    SimpleImputer(strategy="most_frequent"), OneHotEncoder(handle_unknown="ignore")
 )
-    
-cluster_simil= ClusterSimilarity(n_clusters=10, gamma=1.0, random_state=42)
+
+cluster_simil = ClusterSimilarity(n_clusters=10, gamma=1.0, random_state=42)
 
 default_pipeline = make_pipeline(
     SimpleImputer(strategy="median"),
     StandardScaler(),
-)   
+)
 
-preprocessing = ColumnTransformer([
-    ("bedrooms_per_room", ratio_pipeline(), ["total_bedrooms", "total_rooms"]),
-    ("rooms_per_household", ratio_pipeline(), ["total_rooms", "households"]),
-    ("people_per_household", ratio_pipeline(), ["population", "households"]),
-    ("log", log_pipeline, ["total_bedrooms", "total_rooms", "households", 
-                           "population", "median_income"]),
-    ("geo", cluster_simil, ["latitude", "longitude"]),
-    ("cat", cat_pipeline, make_column_selector(dtype_include=object)),
-],
-    remainder=default_pipeline)
+preprocessing = ColumnTransformer(
+    [
+        ("bedrooms_per_room", ratio_pipeline(), ["total_bedrooms", "total_rooms"]),
+        ("rooms_per_household", ratio_pipeline(), ["total_rooms", "households"]),
+        ("people_per_household", ratio_pipeline(), ["population", "households"]),
+        (
+            "log",
+            log_pipeline,
+            [
+                "total_bedrooms",
+                "total_rooms",
+                "households",
+                "population",
+                "median_income",
+            ],
+        ),
+        ("geo", cluster_simil, ["latitude", "longitude"]),
+        ("cat", cat_pipeline, make_column_selector(dtype_include=object)),
+    ],
+    remainder=default_pipeline,
+)
 housing_prepared = preprocessing.fit_transform(housing_train)
 
 
